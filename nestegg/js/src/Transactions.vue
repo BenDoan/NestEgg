38441bfe6ddefec7c45033bf3f868123eb37e796
Ben Doan <ben@bendoan.me>
2016-11-05 16:16:32 -0500

    added transaction display

--- nestegg/js/src/Transactions.vue
+++ nestegg/js/src/Transactions.vue
@@ -1,10 +1,29 @@
 <template>
     <div id="app">
-        <h1>Transactions for {{ global.budget }}</h1>
-        <edi-table v-bind:keys="keys" v-bind:rows="rows"></edi-table>
-        <pre>
-            {{ transactions['total_budgeted'] }}
-        </pre>
+        <h2>Transactions</h2>
+        <table class="table table-hover">
+            <tr>
+                <th></th>
+                <th>Title</th>
+                <th>Amount</th>
+                <th>Date</th>
+                <th>Category</th>
+            </tr>
+            <tr v-for="(transaction, i) in transactions">
+                <td>{{ transaction['title'] }}</td>
+                <td>{{ transaction['amount'] }}</td>
+                <td>{{ transaction['date'] }}</td>
+                <td>{{ transaction['budget_item'].join('/') }}</td>
+            </tr>
+        </table>
+
+        <input type="text" placeholder="Title">
+        <input type="text" placeholder="Amount">
+        <input type="text" placeholder="Date">
+        <select>
+            <option>utilities/gas</option>
+        </select>
+        <button v-on:click="transactions.push({})">Add row</button>
     </div>
 </template>
 <script>
@@ -28,9 +47,14 @@ export default {
        }
     },
     created: function(){
-        this.axios.get("/api/homeinfo/get").then((response) => {
+        this.axios.get("/api/transaction/get/all").then((response) => {
             this.transactions = response.data
         })
     }
 }
 </script>
+
+<style lang="stylus">
+select, button, input
+    color: #000
+</style>
