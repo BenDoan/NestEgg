bb7f83268e53e02e3eb6f76f7aa88f5038701ada
Ben Doan <ben@bendoan.me>
2016-11-05 15:40:56 -0500

    added axios

--- nestegg/js/src/Transactions.vue
+++ nestegg/js/src/Transactions.vue
@@ -2,12 +2,16 @@
     <div id="app">
         <h1>Transactions for {{ global.budget }}</h1>
         <edi-table v-bind:keys="keys" v-bind:rows="rows"></edi-table>
+        <pre>
+            {{ transactions['total_budgeted'] }}
+        </pre>
     </div>
 </template>
 <script>
 import global from './global'
 import Table from './Table.vue'
 export default {
+    name: "transactions",
     components: {
         'edi-table': Table
     },
@@ -19,9 +23,14 @@ export default {
             "amount": "money",
             "desc": "text"
         },
-        rows: []
+        rows: [],
+        transactions: {}
        }
     },
-    name: "transactions"
+    created: function(){
+        this.axios.get("/api/homeinfo/get").then((response) => {
+            this.transactions = response.data
+        })
+    }
 }
 </script>
