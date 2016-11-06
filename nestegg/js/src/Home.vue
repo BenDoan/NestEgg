02d9fb513d7ced1af87d7f1d0527511a91c5fffd
Austin Lubker <Austin@lubkerdesign.com>
2016-11-06 11:48:50 -0600

    Merge

--- nestegg/js/src/Home.vue
+++ nestegg/js/src/Home.vue
@@ -1,118 +1,173 @@
 <template>
-    <div id="app">
-				<div class="holding"><h1 class="main_head">Accounts Overview</h1><div class="line"></div></div>
-				<h2>Checking</h2>
-				<table class="table table-hover">
-					<thead>
-						<tr>
-							<th>Bucket</th>
-							<th>% Used</th>
-						</tr>
-					</thead>
-					<tbody>
-					<tr>
-						<td>Gas</td>
-						<td>20%</td>
-					</tr>
-					<tr>
-						<td>Rent</td>
-						<td>100%</td>
-					</tr>
-					<tr>
-						<td>Gas</td>
-						<td>20%</td>
-					</tr>
-					</tbody>
-
-				</table>
-				<h2>Savings</h2>
-				<table class="table table-hover">
-					<thead>
-						<tr>
-							<th>Bucket</th>
-							<th>% Used</th>
-						</tr>
-					</thead>
-					<tbody>
-					<tr>
-						<td>Gas</td>
-						<td>20%</td>
-					</tr>
-					<tr>
-						<td>Rent</td>
-						<td>100%</td>
-					</tr>
-					</tbody>
-
-				</table>
-				<h2 class="head">Transactions</h2><!--<div class="line"></div>-->
-				<table class="table table-hover">
-					<thead>
-						<tr>
-							<th>#</th>
-							<th>Name</th>
-							<th>Type</th>
-							<th>Amount</th>
-						</tr>
-					</thead>
-					<tbody>
-					<tr>
-						<td>1</td>
-						<td>Transaction Name</td>
-						<td>Type</td>
-						<td>$Amount</td>
-					</tr>
-					<tr>
-						<td>2</td>
-						<td>Transaction Name</td>
-						<td>Type</td>
-						<td>$Amount</td>
-					</tr>
-					<tr>
-						<td>3</td>
-						<td>Transaction Name</td>
-						<td>Type</td>
-						<td>$Amount</td>
-					</tr>
-					<tr>
-						<td>4</td>
-						<td>Transaction Name</td>
-						<td>Type</td>
-						<td>$Amount</td>
-					</tr>
-					<tr>
-						<td>5</td>
-						<td>Transaction Name</td>
-						<td>Type</td>
-						<td>$Amount</td>
-					</tr>
-					</tbody>
-
-				</table>
-    </div>
+<div id="app">
+	<div class="holding"><h1 class="main_head">Accounts Overview</h1><div class="line"></div></div>
+	<h2>Checking</h2>
+	<div class="row">
+		<div class="col-sm-3 bucket-pod">
+			<h2>Bucket Name</h2>
+			<p>Amount Total: $100</p>
+		</div>
+		<div class="col-sm-3 bucket-pod">
+			<h2>Bucket Name</h2>
+			<p>Amount Total: $100</p>
+		</div>
+		<div class="col-sm-3 bucket-pod">
+			<h2>Bucket Name</h2>
+			<p>Amount Total: $100</p>
+		</div>
+	</div>
+	<h2>Savings</h2>
+	<div class="row">
+		<div class="col-sm-3 bucket-pod">
+			<h2>Bucket Name</h2>
+			<p>Amount Total: $100</p>
+		</div>
+		<div class="col-sm-3 bucket-pod">
+			<h2>Bucket Name</h2>
+			<p>Amount Total: $100</p>
+		</div>
+		<div class="col-sm-3 bucket-pod">
+			<h2>Bucket Name</h2>
+			<p>Amount Total: $100</p>
+		</div>
+	</div>
+	<h2 class="head">Transactions</h2>
+	<table class="table table-hover">
+		<thead>
+			<tr>
+				<th>#</th>
+				<th>Name</th>
+				<th>Type</th>
+				<th>Amount</th>
+			</tr>
+		</thead>
+		<tbody>
+		<tr>
+			<td>1</td>
+			<td>Transaction Name</td>
+			<td>Type</td>
+			<td>$Amount</td>
+		</tr>
+		<tr>
+			<td>2</td>
+			<td>Transaction Name</td>
+			<td>Type</td>
+			<td>$Amount</td>
+		</tr>
+		<tr>
+			<td>3</td>
+			<td>Transaction Name</td>
+			<td>Type</td>
+			<td>$Amount</td>
+		</tr>
+		<tr>
+			<td>4</td>
+			<td>Transaction Name</td>
+			<td>Type</td>
+			<td>$Amount</td>
+		</tr>
+		<tr>
+			<td>5</td>
+			<td>Transaction Name</td>
+			<td>Type</td>
+			<td>$Amount</td>
+		</tr>
+		</tbody>
+	</table>
+	<h2 class="head">Budget</h2>
+	<table class="table table-hover">
+		<thead>
+			<tr>
+				<th>#</th>
+				<th>Name</th>
+				<th>Type</th>
+				<th>Amount</th>
+			</tr>
+		</thead>
+		<tbody>
+		<tr>
+			<td>1</td>
+			<td>Transaction Name</td>
+			<td>Type</td>
+			<td>$Amount</td>
+		</tr>
+		<tr>
+			<td>2</td>
+			<td>Transaction Name</td>
+			<td>Type</td>
+			<td>$Amount</td>
+		</tr>
+		<tr>
+			<td>3</td>
+			<td>Transaction Name</td>
+			<td>Type</td>
+			<td>$Amount</td>
+		</tr>
+		<tr>
+			<td>4</td>
+			<td>Transaction Name</td>
+			<td>Type</td>
+			<td>$Amount</td>
+		</tr>
+		<tr>
+			<td>5</td>
+			<td>Transaction Name</td>
+			<td>Type</td>
+			<td>$Amount</td>
+		</tr>
+		</tbody>
+	</table>
+</div>
 </template>
 
-<div class=".col-xs-6 .col-sm-4 bucket-pod">
-	<h2>Bucket Name</h2>
-	<p>Amount Total: 100$</p>
-	<p>80%</p>
+<style lang="stylus">
+/*
+.bar_holder
+background-color:black!important;
+height:20px!important;
+width:100%!important;
 
-</div>
-<div class=".col-xs-6 .col-sm-4 bucket-pod">
-	<h2>Bucket Name</h2>
-	<p>Amount Total: 100$</p>
-	<p>80%</p>
+.progress-bar
+height:20px!important;
+background-color:greent;
+ */
+.row
+	margin-top:60px!important;
+	padding-left:80px;
 
-</div>
-<div class=".col-xs-6 .col-sm-4 bucket-pod">
-	<h2>Bucket Name</h2>
-	<p>Amount Total: 100$</p>
-	<p>80%</p>
+.bucket-pod
+	padding-left:80px;
+	margin-right:60px;
+	border: 1px solid #ccc !important;
+	padding:40px;
+	border-radius:10px;
 
-</div>
+.bucket-pod > h2
+	margin-top:10px;
+
+.bucket-pod:hover
+	background-color:#DDD;
+
+header
+	background: url("../../static/img/bg-pattern.png"), linear-gradient(to left, #4c9760, #60b370)!important;
+
+
+</style>
 
 <script>
 export default {
-    name: "home"
+name: "home",
+    data () {
+       return {
+        global: global,
+        buckets: {},
+        newBucket: ""
+       }
+    },
+    created: function(){
+        this.axios.get("/api/bucket/get/all").then((response) => {
+            this.buckets = response.data
+        })
+    }
 }
 </script>
